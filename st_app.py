d3a096f7e83e2be6873d0860d9ff46fe2967ff87
Carla Pezzone <carlapezzone@gmail.com>
2023-08-27 10:06:12 -0300

    agrego clases y link a repo

--- st_app.py
+++ st_app.py
@@ -2,7 +2,12 @@ import streamlit as st
 from utils import clasificar_texto
 
 # Titulo de la app
-titulo = st.title('Clasificador de textos 🌈⚽️🧬')
+titulo = st.title('Clasificador de textos')
+
+st.write('Este modelo puede clasificar un texto en tres categorías:')
+st.write('🌈 Meteorología')
+st.write('⚽️ Deporte')
+st.write('🧬 Biología')
 
 # Texto que ingresa el usuario
 texto = st.text_input('Ingrese el texto (en inglés) a clasificar aquí: 👇')
@@ -21,6 +26,6 @@ if st.button('Predecir', on_click=clasificar, disabled=len(texto) == 0):
     st.write('Categoría predicha:', f"<span style='color:green'><b>{resultado['categoria']}</b></span>", unsafe_allow_html=True)
     st.write('Probabilidad:', f"<span style='color:green'><b>{probabilidad_formateada}</b></span>", unsafe_allow_html=True)
 
-# Agrega el enlace a LinkedIn
-st.markdown("[![LinkedIn](https://img.shields.io/badge/LinkedIn-blue?style=flat-square&logo=linkedin)](https://www.linkedin.com/in/ingambcarlapezzone/)")
-
+# Agrega el enlace a LinkedIn y github
+st.markdown("Mi contacto en: [![LinkedIn](https://img.shields.io/badge/LinkedIn-blue?style=flat-square&logo=linkedin)](https://www.linkedin.com/in/ingambcarlapezzone/)")
+st.markdown("Mas detalles de este proyecto en: [![Github](https://img.shields.io/badge/GitHub-black?style=flat-square&logo=github)](https://github.com/IngCarlaPezzone/MI_PROYECTO_Clasificador_de_Texto_NLP_FastAPI)")

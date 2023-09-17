a32d3c9e24c8fef04df103203974f2047d758581
Carla Pezzone <carlapezzone@gmail.com>
2023-09-17 16:49:57 -0300

    Arreglando el gitignore

--- Tabla_predicciones.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE predicciones(
-	id SERIAL PRIMARY KEY,
-    texto VARCHAR(255),
-    resultado VARCHAR(50),
-    probabilidad_formateada FLOAT,
-    calificacion VARCHAR(255)
-);
-
-SELECT * FROM predicciones;
